5e17267c4a3f3ec73f765f2fe6620e8e61b859de
lukerhys <hello@lukemelvin.com>
2020-02-23 20:11:03 +0000

    MVP completed with no styling, Adding css.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,42 @@
-document.addEventListener('DOMontentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
 
   const formSubmit = document.querySelector('#new-film-form');
-  formSubmit.addEventListener('submit', handleFromSubmit);
+  formSubmit.addEventListener('submit', handleFormSubmit);
 
+  const deleteAllButton = document.querySelector('#delete-all')
+  deleteAllButton.addEventListener('click', handleDeleteAll);
 });
 
 
 const handleFormSubmit = function () {
   event.preventDefault();
 
-  const list= document.querySelector('#film-lists');
-  const newListItem = document.createElemet('li');
+  const list = document.querySelector('#film-list');
+  const newListItem = document.createElement('li');
 
-  const filmName = documetn.createElemet('h2');
-  filmName.textContent = event.target.filme_name.value;
+  const filmName = document.createElement('h2');
+  filmName.textContent = event.target.film_name.value;
+  newListItem.appendChild(filmName);
 
-  list.appendChild(newListItem)
+  const genre = document.createElement('h4');
+  genre.textContent = event.target.genre.value;
+  newListItem.appendChild(genre);
+
+  const director = document.createElement('h4');
+  director.textContent = event.target.director.value;
+  newListItem.appendChild(director);
+
+  list.appendChild(newListItem);
+
+  form = document.querySelector('#new-film-form');
+    form.reset()
 };
 
+const handleDeleteAll = function (evt) {
+  const filmList = document.querySelector('#film-list');
+  filmList.innerHTML = '';
+}
 // const createListItem = function(element, content, ) {
 //
 // }
